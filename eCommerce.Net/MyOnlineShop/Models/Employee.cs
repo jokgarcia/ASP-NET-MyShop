1b35a4755c2b4d766ac8d8bedd24f268caf9a442
jcgadiano22 <60834988+jcgadiano22@users.noreply.github.com>
2020-02-09 14:43:08 +0800

    Update Employee.cs
    Updated

--- eCommerce.Net/MyOnlineShop/Models/Employee.cs
+++ eCommerce.Net/MyOnlineShop/Models/Employee.cs
@@ -7,5 +7,12 @@ namespace MyOnlineShop.Models
 {
     public class Employee
     {
+        public int EmployeeId { get; set; }
+        public string GivenName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public string MaritalStatus { get; set; }
+
     }
 }
